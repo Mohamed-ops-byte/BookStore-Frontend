97f057cddb6c7af19754eef6f30cfe5d7f27790c
Mohamed Moustafa <mohamed.mostafa22@com.svu.edu.eg>
2026-02-11 12:54:15 +0200

    Connect frontend to Render backend

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 
-const baseUrl = axios.create({ baseURL: "http://127.0.0.1:8000" })
+const baseUrl = axios.create({ baseURL: process.env.REACT_APP_API_URL })
 
 export default baseUrl
